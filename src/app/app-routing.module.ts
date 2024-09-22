181bae863400c7a7d5a13519d44501e167f0ba2e
buitanhieu000@gmail.com <buitanhieu000@gmail.com>
2024-09-23 00:41:12 +0700

    asd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {P001HomeComponent} from "./p-web/pages/p001-home/p001-home.component";
+import { P001HomeComponent } from './p-web/pages/p001-home/p001-home.component';
 
 const routes: Routes = [
   {
-    path:'',
+    path: '',
     component: P001HomeComponent,
-  }
+  },
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/p-web/pages/p004-product-detail/p004-product-detail.component.ts
+++ src/app/p-web/pages/p004-product-detail/p004-product-detail.component.ts
@@ -1,18 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-<<<<<<< HEAD
   selector: 'app-p004-product-detail',
   templateUrl: './p004-product-detail.component.html',
-  styleUrl: './p004-product-detail.component.scss'
+  styleUrl: './p004-product-detail.component.scss',
 })
-export class P004ProductDetailComponent {
-=======
-  selector: 'app-p003-product-detail',
-  templateUrl: './p004-product-detail.component.html',
-  styleUrl: './p004-product-detail.component.scss'
-})
-export class P003ProductDetailComponent {
->>>>>>> a6844c6150e1b2bfd8349c010959782775488bb3
-
-}
+export class P004ProductDetailComponent {}
