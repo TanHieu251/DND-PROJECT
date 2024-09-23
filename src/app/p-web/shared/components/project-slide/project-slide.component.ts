6bfdd2f5a0995ffeab2c2bb1c4cc60dd0509cd3e
buitanhieu000@gmail.com <buitanhieu000@gmail.com>
2024-09-23 22:58:48 +0700

    thêm folder component p-web

---
+++ src/app/p-web/shared/components/project-slide/project-slide.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-slide',
+  templateUrl: './project-slide.component.html',
+  styleUrl: './project-slide.component.scss'
+})
+export class ProjectSlideComponent {
+
+}
