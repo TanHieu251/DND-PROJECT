009882b60ecdaec332d5655f22a7d54141bb539f
lilhanxhardwork04 <lilhanxhardwork04@gmail.com>
2024-11-02 00:25:38 +0700

    update C-cart, C-product, p-header

--- src/app/p-web/shared/components/C-cart/c-cart/c-cart.component.ts
+++ src/app/p-web/shared/components/C-cart/c-cart/c-cart.component.ts
@@ -1,74 +1,4 @@
-// import { Component, EventEmitter, Input, OnInit, Output, ChangeDetectorRef } from '@angular/core';
-// import { productData, serviceData } from '../../../../../data/product';
-// import { ServiceService } from '../../../service/service.service';
 
-// @Component({
-//   selector: 'app-c-cart',
-//   templateUrl: './c-cart.component.html',
-//   styleUrl: './c-cart.component.scss'
-// })
-// export class CCartComponent implements OnInit {
-
-//   @Input() item: any;
-//   @Output() itemRemoved = new EventEmitter<any>();
-//   isCustomerFormVisible = false;
-//   isOrderConfirmed = false;
-//   items = this.cartService.getItems();
-//   totalPrice : number = 0;
-//   totalQuantity : number = 0;
-//   // CartItem = serviceData;
-//   constructor(public cartService: ServiceService, private cdRef: ChangeDetectorRef){}
-//   ngOnInit(): void {
-//     this.updateTotalPriceAndQuantity();
-
-//     this.totalPrice = this.cartService.getTotalPrice();
-//     this.totalQuantity = this.cartService.getTotalQuantity();
-
-//     this.cartService.getCartCountObservable().subscribe(() => {
-//       this.totalQuantity = this.cartService.getTotalQuantity();
-//       this.totalPrice = this.cartService.getTotalPrice();
-//     });
-//   }
-//   updateTotalPriceAndQuantity(){
-//     this.totalPrice = this.getTotalPrice();
-//     this.totalQuantity = this.cartService.getTotalQuantity();
-//   }
-//   //tinh tong gia tien
-//   getTotalPrice(): number{
-//     // return this.cartService.getTotalPrice();
-//       return this.items.reduce((sum, item) => {
-//         const price = item.price || 0;
-//         const quantity = item.quantity || 0;
-//         return sum + (price * quantity);
-//     }, 0);
-//   }
-//   removeItem(item: any) {
-//     this.itemRemoved.emit(this.item);
-//     this.cartService.removeItem(item);
-//     this.updateTotalPriceAndQuantity();
-//   }
-//   toggleCustomerForm(){
-//     this.isCustomerFormVisible = !this.isCustomerFormVisible;
-//     this.isOrderConfirmed = false;
-//   }
-//   confirmCustomerInfor(){
-//     this.isCustomerFormVisible = false;
-//     this.isOrderConfirmed = true;
-//   }
-//     increaseQuantity(item: any) {
-//     item.quantity = (item.quantity || 0) + 1;
-//     this.updateTotalPriceAndQuantity();
-//     this.cdRef.detectChanges();
-//   }
-
-//   decreaseQuantity(item: any) {
-//       if ((item.quantity || 0) > 1) {
-//         item.quantity -= 1;
-//         this.updateTotalPriceAndQuantity();
-//         this.cdRef.detectChanges();
-//     }
-//   }
-// }
 
 import { Component, EventEmitter, Input, OnInit, Output, ChangeDetectorRef } from '@angular/core';
 import { ServiceService } from '../../../service/service.service';
@@ -125,7 +55,7 @@ export class CCartComponent implements OnInit {
     return this.items.reduce((sum, item) => {
       const price = item.price || 0;
       const quantity = item.quantity || 0;
-      console.log(`Item Price: ${price}, Quantity: ${quantity}, Item Total: ${price * quantity}`);
+      // console.log(`Item Price: ${price}, Quantity: ${quantity}, Item Total: ${price * quantity}`);
       return sum + (price * quantity) ;
     }, 0);
   }
